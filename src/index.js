72b4dccfbe9cccd2e426427a3989496a7939a0a7
el634dev <84924260+el634dev@users.noreply.github.com>
2024-09-09 17:35:15 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ class Strings {
         this._string = string
     }
 
+    /**
+     * Returns a string with the first letter capitalized
+     * @param {Strings} str - Local variable used to slice and capitalize the first letter
+     * @returns {Date} - string with the first letter capitalized
+    */
     capitalize(str) {
         const firstLetter = str[0].toUpperCase()
         const lastLetters = str.slice(1)
