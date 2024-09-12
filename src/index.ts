84e873e153f7bb09ab28ee3e51745133adc9ca78
el634dev <84924260+el634dev@users.noreply.github.com>
2024-09-12 10:37:55 -0400

    Added the isEmpty method

--- src/index.ts
+++ src/index.ts
@@ -25,6 +25,24 @@ class Sentence {
     allCaps(strings: string): string {
         return strings.toUpperCase()
     }
+
+     /**
+     * Checks if a string is empty, has whitespace only and matches \n or \r or \t
+     * @param {Sentence} strings - Local variable used to help check the string
+     * @returns {Sentence} - a boolean
+    */
+    isEmpty(strings: string): boolean {
+       for (let i: number = 0; i < strings.length; i += 1) {
+            if (!strings[i] || !strings[i].trim()) {
+                return true
+            } else if (strings[i] === '\n' || strings[i] === '\r' || strings[i] === '\t') {
+                return true;
+            } else {
+                return false
+            }
+       }
+       return true
+    }
 }
 
 module.exports.Sentence = Sentence;
